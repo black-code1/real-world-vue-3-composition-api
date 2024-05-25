4b092cd55a6a83fae5da810426c30b8fea1c5cfd
black-code <legrandmahamat1@gmail.com>
2024-05-25 12:16:23 +0100

    Add Pagination links to the EventList Template and Only show the Next page link when there is a Next page

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,23 +1,33 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService.js'
 import EventCard from '@/components/EventCard.vue'
 
 const props = defineProps(['page']);
 
 const events = ref(null)
+const totalEvents = ref(0)
 
 const page = computed(() => props.page)
 
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvents.value / 2)
+  return page.value < totalPages
+})
+
 onMounted(() => {
+  watchEffect( () => {
+    events.value = null
   EventService
     .getEvents(2, page.value)
     .then((response) => {
       events.value = response.data
+      totalEvents.value = response.headers['x-total-count']
     })
     .catch((error) => {
       console.log(error)
     })
+  })
 })
 </script>
 
@@ -25,6 +35,18 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
+
+    <router-link
+      :to="{ name: 'event-list', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+      >Prev Page</router-link>
+
+    <router-link
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="nex"
+      v-if="hasNextPage"
+      >Next Page</router-link>
   </div>
 </template>
 
